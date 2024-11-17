2216be33221721361c84b1a84e6944e0810047bf
Reynaldo Solís <48728298+solisrey22@users.noreply.github.com>
2024-11-17 15:45:08 -0600

    Carga loader

--- script.js
+++ script.js
@@ -16,27 +16,13 @@ function seleccionar(){
     menuVisible = false;
 }
 
-//Funcion que aplica las animaciones de las habilidades
-function efectoHabilidades(){
-    var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distancia_skills >= 300){
-        let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
-        habilidades[5].classList.add("comunicacion");
-        habilidades[6].classList.add("trabajo");
-        habilidades[7].classList.add("creatividad");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
-    }
+const cargar = document.querySelector(".cargar");
+window.onload = function(){
+  setTimeout(function(){
+    cargar.style.opacity = "0";
+    setTimeout(function(){
+      cargar.style.display = "none";
+    }, 500);
+  },1500);
 }
 
-
-//detecto el scrolling para aplicar la animacion de la barra de habilidades
-window.onscroll = function(){
-    efectoHabilidades();
-} 
